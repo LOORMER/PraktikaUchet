40e99d5cc950163a01940e3be084959e779efa07
farhod849 <farhodsabirov42@gmail.com>
2025-02-08 09:09:34 +0300

    commit #1

---
+++ PrakrikaUP04.01/appData/Connect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrakrikaUP04._01.appData
+{
+    internal class Connect
+    {
+        public static PraktikaProninEntities c;
+        public static PraktikaProninEntities context
+        {
+            get
+            {
+                if(c == null)
+                    c = new PraktikaProninEntities();
+                return c;
+            }
+        }
+    }
+    
+}
---
+++ PrakrikaUP04.01/appData/Nav.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PrakrikaUP04._01.appData
+{
+    internal class Nav
+    {
+        public static Frame MainFrame;
+    }
+}
